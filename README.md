72ae6c5b7e131e2d3050cd31b36991a556b16b3e
Green <40367733+greenlucid@users.noreply.github.com>
2022-08-06 12:46:28 +0200

    init

---
+++ README.md
@@ -0,0 +1,47 @@
+# poh-briber
+
+Experimental project to reward voters that delegate to a briber delegate in PoH.
+
+## Concept
+
+- PoH users (bribees) delegate to a delegate (briber).
+- The briber creates auctions for every vote.
+- The winner of the auction can decide the option to vote.
+- The briber votes for this option in Snapshot.
+- After the vote is over, the briber calls a function in the contract to distribute rewards. In this function, the addresses of the bribees are passed as calldata.
+- - A small amount goes to the briber in the concept as a dev fee, and especially, to compensate the human effort of creating the auctions and calling the functions.
+- - A significant amount is devoted to contribute to the DAO.
+- - The remainder is used to reward the bribees.
+
+Delegating the vote is not enough to be accepted as a bribee, it is also needed to not have actually voted, to make sure the delegator does not override the vote.
+
+The frontend will generate this list of valid bribees for reward distribution.
+
+## Caveats
+
+- Requires manual effort for the briber, who has to create the auctions and send the rewards.
+- Crucial steps in the process are centralized.
+- - Briber creates the auctions.
+- - Briber votes the chosen option in Snapshot.
+- - Briber distributes the rewards, choosing arbitrary addresses.
+
+Thus, it requires a trusted briber, that ideally holds reputation within the community.
+
+## Other details
+
+The auction is a generalized second-price auction (GSP).
+
+Harberger Taxes, while interesting, are not being used, because there are issues around keeping track of what bounty should be paid on each vote.
+
+## Rationale
+
+There are different ways of bribing.
+
+One way is, per Snapshot vote, put a bounty for each option. The option that wins has its bounty distributed.
+There are two alternatives from here:
+1. Loser options get the pools refunded to the donors. But this is a bit cumbersome to implement, because you then need to identify the donations and remember the donor per donation.
+2. Loser options also are distributed among those that voted for them.
+
+This route makes voters that don't necessarily require the bribe, also get the reward. So, it's less efficient in the sense that the subset of voters that are only interested in the bribe could get a bigger reward.
+
+Self selecting as a bribee is easy: just delegate to the briber delegate, and don't overwrite your vote. Also, implementing this as an auction is easier, and arguably more fun, so I went with it.
---
+++ contracts/contracts/PohBriber.sol
@@ -0,0 +1,95 @@
+/**
+ * @authors: [@greenlucid]
+ * @reviewers: []
+ * @auditors: []
+ * @bounties: []
+ * @deployments: []
+ * SPDX-License-Identifier: Licenses are not real
+ */
+
+pragma solidity ^0.8.14;
+
+contract PohBriber {
+  
+  struct Auction {
+    string metadata; // info about the Snapshot vote, title, etc. uri to ipfs file?
+    address winner;
+    uint256 option;
+    uint256 highBid;
+    uint256 secondBid;
+    uint256 auctionDeadline;
+    uint256 optionDeadline;
+    bool distributed;
+  }
+  
+  address public constant briber = 0x89c4ACb8B5b5B8E5B2121934B9e143569a914C80;
+  address public constant dao = 0x89c4ACb8B5b5B8E5B2121934B9e143569a914C80; // todo get right
+
+  uint256 public constant briberPercent = 5; // compensate briber for handling the contract.
+  uint256 public constant daoPercent = 45; // send a generous amount to the dao.
+  uint256 public constant bribePercent = 50; // reward for the bribees.
+
+  Auction[] public auctions;
+
+  modifier onlyBriber() {
+    require(msg.sender == briber, "Only briber");
+    _;
+  }
+
+  function createAuction(
+      string calldata _metadata, uint256 _auctionPeriod, uint256 _optionPeriod
+  ) external onlyBriber {
+
+    Auction memory auction = Auction(
+      _metadata,
+      briber,
+      0, 0, 0,
+      block.timestamp + _auctionPeriod,
+      block.timestamp + _auctionPeriod + _optionPeriod,
+      false
+    );
+
+    auctions.push(auction);
+  }
+
+  function bid(uint256 _auctionId) external payable {
+    require(_auctionId < auctions.length, "Auction does not exist");
+    Auction storage auction = auctions[_auctionId];
+    require(auction.auctionDeadline >= block.timestamp, "Too late");
+    require(msg.value > auction.highBid, "Too low");
+
+    payable(auction.winner).send(auction.highBid);
+    auction.secondBid = auction.highBid;
+    auction.highBid = msg.value;
+    auction.winner = msg.sender;
+  }
+
+  function setOption(uint256 _auctionId, uint256 _option) external {
+    require(_auctionId < auctions.length, "Auction does not exist");
+    Auction storage auction = auctions[_auctionId];
+    require(auction.optionDeadline >= block.timestamp, "Too late");
+    require(auction.winner == msg.sender, "Only auction winner");
+    auction.option = _option;
+  }
+
+
+  /// @param _bribees If this is too large, it may exceed block size limit.
+  /// If this ever happens, briber (who should be reputable, anyway),
+  /// should call if with only themselves as _bribees, and then figure out
+  /// how to distribute it later.
+  function distributeRewards(uint256 _auctionId, address[] calldata _bribees) external onlyBriber {
+    require(_auctionId < auctions.length, "Auction does not exist");
+    Auction storage auction = auctions[_auctionId];
+    // 1. send the second price surplus to auction winner
+    payable(auction.winner).send(auction.highBid - auction.secondBid);
+    // 2. the secondBid is the "real amount" we distribute. send fees.
+    payable(briber).send(auction.secondBid * briberPercent / 100);
+    payable(dao).send(auction.secondBid * daoPercent / 100);
+    // 3. send the actual bribes. (shameful, lazy for loop...)
+    uint256 i;
+    for (i = 0; i < _bribees.length; i++) {
+      payable(_bribees[i]).send(auction.secondBid / _bribees.length * bribePercent / 100);
+    }
+    auction.distributed = true;
+  }
+}
